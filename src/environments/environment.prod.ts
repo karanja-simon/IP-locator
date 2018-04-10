46d788302383e7402e79fb228769854ff0e29be5
Karanja Simon <karanja-simon@users.noreply.github.com>
2018-04-10 14:13:02 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  MAPBOX_API_KEY: 'pk.eyJ1Ijoic2ltb25raXJ1a3UiLCJhIjoiY2pmNm8zYnF4MHJwYTJ2bHBhNjM2NmJvaiJ9.9vN7xhxzYqz7Af9UQBAbig'
+  MAPBOX_API_KEY: 'your key here'
 };
